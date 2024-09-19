1cd66cbd6e2b9510e89829652edc895dbf3a0420
jwz0703 <ilovetaiwan703@gmail.com>
2024-09-20 01:57:34 +0800

    first commit

---
+++ push.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo "# launch" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jwz0703/launch.git
+git push -u origin main
+pause
